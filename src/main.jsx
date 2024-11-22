07bab4b35507d2397d4c4c9caa16c45403ce7470
Manvendra <97372903+Manvendra200125@users.noreply.github.com>
2024-11-22 23:52:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Tictactoc } from './components/Tictactoc.jsx'; 
+import { Tictactoc } from './Components/Tictactoc'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Tictactoc/>
-
   </StrictMode>,
 )
